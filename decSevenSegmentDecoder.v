6867dfc7f4b314c9cdc200978951798a7e447245
BenBTurtle <benbturtle@gmail.com>
2025-02-25 16:42:05 -0500

    FPGA top level module completed and tested

--- decSevenSegmentDecoder.v
+++ decSevenSegmentDecoder.v
@@ -1,24 +1,64 @@
-module decSevenSegmentDecoder(input [3:0] in,
-output [7:0] out);
+module fiveBitSevenSegmentDecoder(input [4:0] in,
+output [7:0] disp1,
+output [7:0] disp0);
 
-reg [7:0] wout;
+reg [7:0] wout1;
+reg [7:0] wout0;
 
+
+//This is a brute force implementation, Feel free to update this later with a less brute force solution
 always@ (*) begin
-	case(in)
-		4'h0: wout = 8'b11000000;
-		4'h1: wout = 8'b11111001;
-		4'h2: wout = 8'b10100100;
-		4'h3: wout = 8'b10110000; 
-		4'h4: wout = 8'b10011001; 
-		4'h5: wout = 8'b10010010; 
-		4'h6: wout = 8'b10000010; 
-		4'h7: wout = 8'b11111000; 
-		4'h8: wout = 8'b10000000;
-		4'h9: wout = 8'b10010000;
-		default: wout = 8'b01111111; //invalid returns a decimal place only
+	case(in) //NOTE: wout1/disp1 = hex1 and wout0/disp0 = hex0 on MAX10 FPGA
+		0: begin wout1 = 8'b11000000; wout0 = 8'b11000000; end
+		1: begin wout1 = 8'b11000000; wout0 = 8'b11111001; end
+		2: begin wout1 = 8'b11000000; wout0 = 8'b10100100; end
+		3: begin wout1 = 8'b11000000; wout0 = 8'b10110000; end
+		4: begin wout1 = 8'b11000000; wout0 = 8'b10011001; end
+		5: begin wout1 = 8'b11000000; wout0 = 8'b10010010; end
+		6: begin wout1 = 8'b11000000; wout0 = 8'b10000010; end
+		7: begin wout1 = 8'b11000000; wout0 = 8'b11111000; end
+		8: begin wout1 = 8'b11000000; wout0 = 8'b10000000; end
+		9: begin wout1 = 8'b11000000; wout0 = 8'b10010000; end
+		10: begin wout1 = 8'b11111001; wout0 = 8'b11000000; end
+		11: begin wout1 = 8'b11111001; wout0 = 8'b11111001; end
+		12: begin wout1 = 8'b11111001; wout0 = 8'b10100100; end
+		13: begin wout1 = 8'b11111001; wout0 = 8'b10110000; end
+		14: begin wout1 = 8'b11111001; wout0 = 8'b10011001; end
+		15: begin wout1 = 8'b11111001; wout0 = 8'b10010010; end
+		16: begin wout1 = 8'b11111001; wout0 = 8'b10000010; end
+		17: begin wout1 = 8'b11111001; wout0 = 8'b11111000; end
+		18: begin wout1 = 8'b11111001; wout0 = 8'b10000000; end
+		19: begin wout1 = 8'b11111001; wout0 = 8'b10010000; end
+		20: begin wout1 = 8'b10100100; wout0 = 8'b11000000; end
+		21: begin wout1 = 8'b10100100; wout0 = 8'b11111001; end
+		22: begin wout1 = 8'b10100100; wout0 = 8'b10100100; end
+		23: begin wout1 = 8'b10100100; wout0 = 8'b10110000; end
+		24: begin wout1 = 8'b10100100; wout0 = 8'b10011001; end
+		25: begin wout1 = 8'b10100100; wout0 = 8'b10010010; end
+		26: begin wout1 = 8'b10100100; wout0 = 8'b10000010; end
+		27: begin wout1 = 8'b10100100; wout0 = 8'b11111000; end
+		28: begin wout1 = 8'b10100100; wout0 = 8'b10000000; end
+		29: begin wout1 = 8'b10100100; wout0 = 8'b10010000; end
+		30: begin wout1 = 8'b10110000; wout0 = 8'b11000000; end
+		31: begin wout1 = 8'b10110000; wout0 = 8'b11111001; end
+		default: begin wout1 = 8'b01111111; wout0 = 8'b01111111; end //invalid returns a decimal place only
 	endcase
 end
 
-assign out = wout;
+assign disp1 = wout1;
+assign disp0 = wout0;
 
 endmodule
+
+/* Reference table dec
+0 = 8'b11000000
+1 = 8'b11111001
+2 = 8'b10100100
+3 = 8'b10110000
+4 = 8'b10011001
+5 = 8'b10010010
+6 = 8'b10000010
+7 = 8'b11111000
+8 = 8'b10000000
+9 = 8'b10010000
+end reference table */
--- gcd.v
+++ gcd.v
@@ -4,25 +4,97 @@ input load,
 input [4:0] aIn,
 input [4:0] bIn,
 output reg [4:0] result,
-output [15:0] disp,
-output reg done);
+output [7:0] disp0,
+output [7:0] disp1,
+output reg done,
+output reg debug);
 
 reg [4:0] a, b; 
 wire [4:0] mOut, resCheck;
-
-initial begin
-result <= 5'b00000;
-a <= 5'b00000;
-b <= 5'b00000;
-done <= 1'b0;
-end
+wire [1:0] state = {reset, load}; //remember to default these to 1
 
 min m(a,b,mOut);
+max maout(a,b,resCheck);
+
+always@ (clk) begin
+
+	case(state)
+		2'b11 : begin
+			if (mOut == 5'b00000) begin
+				result <= resCheck;
+				a <= a;
+				b <= b;
+				done <= 1'b1;
+			end else begin
+				if (a>=b) begin
+					a <= a-b;
+					b <=b;
+				end else begin
+					a <= a;
+					b <= b-a;
+				end
+			end
+		end
+		2'b01 : begin
+			a <= 5'b00000;
+			b <= 5'b00000;
+			done <= 1'b0;
+		end
+		2'b10 : begin
+			a <= aIn;
+			b <= bIn;
+			done <= 1'b0;
+		end
+		2'b00 : begin
+			a <= 5'b00000;
+			b <= 5'b00000;
+			done <= 1'b0;
+		end
+		default : begin
+			a <= 5'b00000;
+			b <= 5'b00000;
+			done <= 1'b0;
+			debug <= 1'b1;
+		end	
+	endcase
+end
 
 
 
-always@ (posedge clk) begin
+fiveBitSevenSegmentDecoder displayResults(result, disp1, disp0);
 
+endmodule
+
+
+module gcd_TB();
+//inputs
+reg clk, reset, load;
+reg [4:0] aIn, bIn;
+//outputs
+wire [4:0] result;
+wire done;
+wire [15:0] disp;
+
+gcd DUT(clk, reset, load, aIn, bIn, result, disp, done);
+
+always begin
+	#2 clk = ~clk;
+end
+
+initial begin
+	clk = 0; reset = 1; load = 0; aIn = 30; bIn = 10; #10;
+	reset = 0; load = 0; aIn = 30; bIn = 10; #10; //check for reset
+	reset = 1; load = 0; aIn = 15; bIn = 25; #10; //b>a
+	reset = 1; load = 1; aIn = 0; bIn = 10; #10; //check for correct load
+	reset = 1; load = 0; aIn = 0; bIn = 10; #10; //now actually load
+	$stop;
+end
+
+endmodule
+
+
+
+/* OLD non functional design in case I need it again
 //sets only if button is pressed
 	a <= (load==0)? aIn : a;
 	b <= (load==0)? bIn : b;
@@ -54,35 +126,4 @@ end else begin
 	done <= 1'b0;
 end
 	result <= (done == 1)? resCheck : result;
-end
-
-max maout(a,b,resCheck);
-
-endmodule
-
-
-module gcd_TB();
-//inputs
-reg clk, reset, load;
-reg [4:0] aIn, bIn;
-//outputs
-wire [4:0] result;
-wire done;
-wire [15:0] disp;
-
-gcd DUT(clk, reset, load, aIn, bIn, result, disp, done);
-
-always begin
-	#2 clk = ~clk;
-end
-
-initial begin
-	clk = 0; reset = 1; load = 0; aIn = 30; bIn = 10; #10;
-	reset = 0; load = 0; aIn = 30; bIn = 10; #10; //check for reset
-	reset = 1; load = 0; aIn = 15; bIn = 25; #10; //b>a
-	reset = 1; load = 1; aIn = 0; bIn = 10; #10; //check for correct load
-	reset = 1; load = 0; aIn = 0; bIn = 10; #10; //now actually load
-	$stop;
-end
-
-endmodule
+*/
